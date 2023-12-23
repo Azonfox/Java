143fe540699f536c6b7feed464c9df8bfe9524fd
Azonfox <azonfox@gmail.com>
2023-12-23 20:00:59 +0600

    Fix project

--- dudar/sample/Main_old.java
+++ dudar/sample/Main_old.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -14,6 +15,7 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 747, 460));
         primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
