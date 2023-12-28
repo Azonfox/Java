cd6a33ef255ba119f33614c0f9618ed329387282
Azonfox <azonfox@gmail.com>
2023-12-28 18:03:58 +0600

    Shell, LIB

---
+++ test/lib/mylib.java
@@ -0,0 +1,7 @@
+package lib;
+
+public class mylib {
+    public static void printstr() {
+        System.out.printf("%n%nMyLib it Work in /lib/mylib.class%n%n");
+    }
+}
--- test/mypact/test.java
+++ test/mypact/test.java
@@ -1,7 +1,12 @@
-package ha;
+package mypact;
+
 import java.util.Scanner;
+import lib.mylib;
+
 public class test {
+
     public static void main(String[] args) {
+      mylib.printstr();
         String aaa="ABC=";
         float bbb=14;
         System.out.println("Az  "+aaa+bbb);
@@ -12,6 +17,5 @@ public class test {
         Scanner input = new Scanner(System.in);
         int myInt = input.nextInt();
         System.out.printf("%nВведено:  %d.%n%n",myInt);
-
     }
 }
