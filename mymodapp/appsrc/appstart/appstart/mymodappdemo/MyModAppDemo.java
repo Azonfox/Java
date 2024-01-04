6f9b9b4409e1c9dabc185d6d5e1c793797716377
Azonfox <azonfox@gmail.com>
2024-01-04 21:37:20 +0600

    Modules MyModApp16 Shildt10

---
+++ mymodapp/appsrc/appstart/appstart/mymodappdemo/MyModAppDemo.java
@@ -0,0 +1,18 @@
+// Demonstrate a simple module-based application.
+package appstart.mymodappdemo;
+
+import appfuncs.simplefuncs.SimpleMathFuncs;
+
+public class MyModAppDemo {
+  public static void main(String[] args) {
+
+    if(SimpleMathFuncs.isFactor(2, 10))
+      System.out.println("2 множитель 10");
+
+    System.out.println("Наименьший общий множитель 35 и 105 - " +
+                       SimpleMathFuncs.lcf(35, 105));
+
+    System.out.println("Наибольший общий множитель 35 и 105 - " +
+                       SimpleMathFuncs.gcf(35, 105));
+  }
+}
