1aef06f69d2995be1eab156a7fa8156c58526ef1
Azonfox <azonfox@gmail.com>
2024-01-25 14:09:11 +0600

    new Project TestWrite

--- SDB/dbfields.java
+++ SDB/dbfields.java
@@ -1,20 +1,38 @@
 // Test записи данных по определенным позициям в файл
 import java.io.*;
 
+class dbHead{
+    byte vers;	  // Версия 
+    int datapos;  // Адрес начала данных
+    int recleng;  // Длина записи с флагом
+    int reccount; // Количество записей в базе
+    int reserv=0; // Резерв
+
+
+}
+
+
 class Field {
     int recpos=0;
     int pos;
     int lengh;
     String name = new String();
     String data = new String();
-    String tmp = new String();
+    String  tmp = new String();
+
+    void setField(int recpos,int pos, int lengh,String name,String data){
+        this.recpos=recpos;
+        this.pos=pos;
+        this.lengh=lengh;
+        this.name=name;
+        this.data=data;    
+    }
 
     void write()  throws IOException {
       try (RandomAccessFile fout = new RandomAccessFile("dbf1.txt","rw")) {
 	fout.seek(recpos+pos);
 	for(int i=0; i<lengh;i++) tmp+='-';
 	fout.writeUTF((data+tmp).substring(0,lengh)); 
-
 	fout.close(); // Закрыть файл!
       } catch(IOException e) {
       System.out.println("I/O Error: " + e);
@@ -26,7 +44,6 @@ class Field {
       try (RandomAccessFile fout = new RandomAccessFile("dbf1.txt","r")) {
 	fout.seek(recpos+pos);
 	data=fout.readUTF(); 
-
 	fout.close(); // Закрыть файл!
       } catch(IOException e) {
         System.out.println("I/O Error: " + e);
@@ -42,16 +59,35 @@ class Field {
     }
 }
 
+class dbrecord {
+      Field fieldx = new Field();
+
+    void writeRecord()  throws IOException {
+      fieldx.setField(0,150,26,"Имя3","Record-Новые Данные"); 
+      fieldx.write();
+    }
+
+    void printRecord()  throws IOException {
+      fieldx.setField(0,150,26,"Имя3","Record-Новые Данные"); 
+      fieldx.read();
+      fieldx.print();
+    }
+}
+
 class dbfields {
   public static void main(String args[]) throws IOException {
 
     Field fields = new Field();
-        fields.recpos=0;
-        fields.pos=0;
-        fields.lengh=50;
-        fields.name="Имя";
-        fields.data="Новые Данные";    
-    
+    dbrecord recx = new dbrecord();
+    recx.writeRecord();
+    recx.printRecord();
+
+    fields.setField(0,0,26,"Имя1","data-Новые Данные"); 
+	fields.write();
+	fields.read();
+	fields.print();
+
+    fields.setField(0,64,26,"Имя2","DANABASE-Новые Данные"); 
 	fields.write();
 	fields.read();
 	fields.print();
---
+++ write/testwrite.java
@@ -0,0 +1,48 @@
+// Test записи различных данных по определенным позициям в файл
+import java.io.*;
+
+class testwrite {
+  public static void main(String args[]) throws IOException {
+    byte   b=10;
+    short  s=500;
+    int    i=9876;
+    long   l=123456;
+    float  f=1234.56789F;
+    double d=1234.56789D;
+    char   c='А';
+    String buffer  = new String();
+    buffer="BSILFDC* - тестовая строка";
+
+    File filedel = new File("dbf1.txt");
+    if(filedel.delete()){
+        System.out.println("Файл данных удален-очистка!");
+    }
+
+    try (RandomAccessFile fout = new RandomAccessFile("dbf1.txt","rw")) {
+	// Запись различных данных по своим местам
+	fout.seek(0);   fout.writeByte(b); 
+	fout.seek(16); 	fout.writeShort(s); 
+	fout.seek(32); 	fout.writeInt(i); 
+	fout.seek(48); 	fout.writeLong(l); 
+	fout.seek(64); 	fout.writeFloat(f); 
+	fout.seek(80); 	fout.writeDouble(d); 
+	fout.seek(96); 	fout.writeChar(c); 
+	fout.seek(112);	fout.writeUTF(buffer); 
+
+	// Читаем данные из своих мест и печатаем их
+        System.out.println("Читаем данные:");
+	fout.seek(0);   System.out.println(fout.readByte()); 
+	fout.seek(16); 	System.out.println(fout.readShort()); 
+	fout.seek(32); 	System.out.println(fout.readInt()); 
+	fout.seek(48); 	System.out.println(fout.readLong()); 
+	fout.seek(64); 	System.out.println(fout.readFloat()); 
+	fout.seek(80); 	System.out.println(fout.readDouble()); 
+	fout.seek(96); 	System.out.println(fout.readChar()); 
+	fout.seek(112);	System.out.println(fout.readUTF()); 
+
+	fout.close(); // Закрыть файл!
+    } catch(IOException e) {
+      	System.out.println("I/O Error: " + e);
+    }
+  }
+}
